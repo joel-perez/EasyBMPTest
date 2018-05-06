d3d388f557bbf1a08102207ec50e286b393162ad
joel <joel@JoelUbuntu>
2018-05-06 18:55:05 -0300

    Agrego cambios para que me compile en mi ubuntu

--- src/EasyBMPTest.h
+++ src/EasyBMPTest.h
@@ -1,9 +1,9 @@
 #ifndef EASYBMPTEST_H_
 #define EASYBMPTEST_H_
 
-#include "lib\EasyBMP.h"
-#include "lib\EasyBMP_Geometry.h"
-#include "lib\EasyBMP_Font.h"
+#include "lib/EasyBMP.h"
+#include "lib/EasyBMP_Geometry.h"
+#include "lib/EasyBMP_Font.h"
 
 void crearEjemploTexto();
 void crearEjemploTerreno();
